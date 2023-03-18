f389fcf7db6857759f0ff239827bf7b3ce23d802
Vitor Fel <vitorfel2000@gmail.com>
2023-03-18 20:48:08 -0300

    :construction: Building components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
-import { gql } from "@apollo/client/core";
-import { useQuery } from "@apollo/client/react";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface LessonInterface {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY);
-
   return (
-    <ul>
-      {data?.lessons.map((lesson: LessonInterface) => {
-        return <li key={lesson.id}>{ lesson.title }</li>
-      })}
-    </ul>
+    <Event />
   );
 }
 
